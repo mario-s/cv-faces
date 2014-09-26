fa31a6cf8a68f956e1a98b323b95a3306e39eed1
Mario-S <spindizzy@outlook.com>
2014-09-26 15:11:40 +0200

    cahnged creation of worker

--- src/main/java/org/opencv/face/VideoWindow.java
+++ src/main/java/org/opencv/face/VideoWindow.java
@@ -22,11 +22,14 @@ public class VideoWindow extends JFrame {
 
     private final VideoPanel videoPanel;
 
+    private SwingWorker worker;
+
     public VideoWindow() {
         super("Face Detection");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         faceDetector = new FaceDetector();
         videoPanel = new VideoPanel();
+        worker = createWorker();
         getContentPane().add(videoPanel, BorderLayout.CENTER);
         pack();
         setSize(400, 400);
@@ -36,49 +39,41 @@ public class VideoWindow extends JFrame {
 
             @Override
             public void windowClosing(WindowEvent e) {
+                worker.cancel(true);
                 dispose();
                 System.exit(0);
             }
 
         });
 
-        try {
-            readVideoStream();
-        } catch (Exception e) {
-            LOG.error(e.getMessage(), e);
-            dispose();
-        }
+        worker.execute();
     }
 
-    private void readVideoStream() throws InterruptedException {
+    private SwingWorker createWorker() {
+        return new SwingWorker() {
 
-        final VideoCapture capture = new VideoCapture(0);
-        Thread.sleep(1000);
-        if (capture.isOpened()) {
-            SwingWorker worker = new SwingWorker() {
+            @Override
+            protected Object doInBackground() throws Exception {
+                final VideoCapture capture = new VideoCapture(0);
+                Thread.sleep(1000);
 
-                @Override
-                protected Object doInBackground() throws Exception {
-                    while (true) {
-                        Mat webcamImage = new Mat();
-                        capture.read(webcamImage);
-                        if (!webcamImage.empty()) {
-                            setSize(webcamImage.width()+40,webcamImage.height()+60);  
-                            //-- 3. Apply the classifier to the captured image  
-                            faceDetector.markFaces(webcamImage);
-                            //-- 4. Display the image  
-                            videoPanel.updateImage(webcamImage);
-                        } else {
-                            LOG.warn(" --(!) No captured frame -- Break!");
-                            break;
-                        }
+                while (true) {
+                    Mat webcamImage = new Mat();
+                    capture.read(webcamImage);
+                    if (!webcamImage.empty()) {
+                        setSize(webcamImage.width() + 40, webcamImage.height() + 60);
+                        //-- 3. Apply the classifier to the captured image  
+                        faceDetector.markFaces(webcamImage);
+                        //-- 4. Display the image  
+                        videoPanel.updateImage(webcamImage);
+                    } else {
+                        LOG.warn(" --(!) No captured frame -- Break!");
+                        break;
                     }
-                    return null;
                 }
-            };
-            worker.execute();
-
-        }
+                return null;
+            }
+        };
     }
 
 }
