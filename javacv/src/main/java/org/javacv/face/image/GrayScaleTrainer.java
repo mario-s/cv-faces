bfccd45de412481a7d5fb2e185ccdb03033bd492
Mario-S <spindizzy@outlook.com>
2014-12-27 22:18:53 +0100

    upgraded javacv

--- javacv/src/main/java/org/javacv/face/image/GrayScaleTrainer.java
+++ javacv/src/main/java/org/javacv/face/image/GrayScaleTrainer.java
@@ -2,7 +2,7 @@ package org.javacv.face.image;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import org.bytedeco.javacpp.opencv_core;
+import java.nio.IntBuffer;
 import static org.bytedeco.javacpp.opencv_core.CV_32SC1;
 import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_core.MatVector;
@@ -32,13 +32,14 @@ public class GrayScaleTrainer implements Trainable{
         File[] imageFiles = root.listFiles(filter);
         MatVector images = new MatVector(imageFiles.length);
         Mat labels = new Mat(imageFiles.length, 1, CV_32SC1);
+        IntBuffer buffer = labels.createBuffer();
         int counter = 0;
 
         for (File image : imageFiles) {
             Mat img = imread(image.getAbsolutePath(), CV_LOAD_IMAGE_GRAYSCALE);
             int label = Integer.parseInt(image.getName().split("\\-")[0]);
             images.put(counter, img);
-            labels.getIntBuffer().put(counter, label);
+            buffer.put(counter, label);
             counter++;
         }
         
