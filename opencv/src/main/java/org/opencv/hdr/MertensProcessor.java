3a496613e0f1f089a7bb3bda9f0a9ed842258bb0
Mario-S <spindizzy@outlook.com>
2016-02-20 11:29:05 -0500

    removed unused imports

--- opencv/src/main/java/org/opencv/hdr/MertensProcessor.java
+++ opencv/src/main/java/org/opencv/hdr/MertensProcessor.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
