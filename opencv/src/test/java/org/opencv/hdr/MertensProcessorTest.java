2b57a042c29252e9dfc7e804923de95dde715edc
Mario-S <spindizzy@outlook.com>
2018-04-24 21:43:32 +0200

    test update

--- opencv/src/test/java/org/opencv/hdr/MertensProcessorTest.java
+++ opencv/src/test/java/org/opencv/hdr/MertensProcessorTest.java
@@ -3,12 +3,16 @@ package org.opencv.hdr;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
+import static org.hamcrest.CoreMatchers.is;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
 
 /**
  *
@@ -41,8 +45,7 @@ public class MertensProcessorTest {
      * Test of create method, of class MertensProcessor.
      */
     @Test
-    @Ignore
-    public void testCreate_Empty() {
+    public void create_Empty() {
         assertFalse(classUnderTest.create(images, out));
     }
     
@@ -50,8 +53,7 @@ public class MertensProcessorTest {
      * Test of create method, of class MertensProcessor.
      */
     @Test
-    @Ignore
-    public void testCreate_Memorial() {
+    public void create_Memorial() {
         images.add(getClass().getResource("memorial0061.png").getFile());
         images.add(getClass().getResource("memorial0064.png").getFile());
         images.add(getClass().getResource("memorial0067.png").getFile());
@@ -61,13 +63,22 @@ public class MertensProcessorTest {
     }
     
     @Test
-    public void testCreate_Pic() {
+    public void create_Pic() {
         images.add(getClass().getResource("Picture_201508010708_0.jpg").getFile());
         images.add(getClass().getResource("Picture_201508010708_1.jpg").getFile());
         images.add(getClass().getResource("Picture_201508010708_2.jpg").getFile());
         
         assertTrue(classUnderTest.create(images, out));
         assertTrue(out.exists());
+    }
+    
+    @Test
+    public void multiply() {
+        Mat src = Imgcodecs.imread(getClass().getResource("Picture_201508010708_0.jpg").getFile());
+        Scalar s = new Scalar(255,0,255);
+        Mat filter = new Mat(src.rows(), src.cols(), src.type(), s);
+        Mat dest = src.mul(filter);
+        assertThat(dest.empty(), is(false));
     }
     
 }
