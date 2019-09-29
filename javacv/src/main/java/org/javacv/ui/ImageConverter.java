a3caa06a503cc42a7fb408eca15e2b017197172b
Mario-S <spindizzy@outlook.com>
2019-09-29 21:24:44 +0200

    cleanup

--- javacv/src/main/java/org/javacv/ui/ImageConverter.java
+++ javacv/src/main/java/org/javacv/ui/ImageConverter.java
@@ -15,19 +15,22 @@ final class ImageConverter {
     private ImageConverter() {
     }
 
-    public static BufferedImage toBufferedImage(Mat matrix) {
-        int type = BufferedImage.TYPE_BYTE_GRAY;
-        if (matrix.channels() > 1) {
-            type = BufferedImage.TYPE_3BYTE_BGR;
-        }
-        byte[] bytes = readPixel(matrix);
-        BufferedImage image = new BufferedImage(matrix.cols(), matrix.rows(), type);
+    public static BufferedImage toBufferedImage(final Mat matrix) {
+        final BufferedImage image = new BufferedImage(matrix.cols(), matrix.rows(), getType(matrix));
         final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        byte[] bytes = readPixel(matrix);
         System.arraycopy(bytes, 0, targetPixels, 0, bytes.length);
         return image;
     }
 
-    private static byte[] readPixel(Mat matrix) {
+    private static final int getType(final Mat matrix) {
+        if (matrix.channels() > 1) {
+            return BufferedImage.TYPE_3BYTE_BGR;
+        }
+        return BufferedImage.TYPE_BYTE_GRAY;
+    }
+
+    private static final byte[] readPixel(final Mat matrix) {
         byte[] bytes = new byte[matrix.channels() * matrix.cols() * matrix.rows()];
         BytePointer pointer = matrix.data();
         pointer.get(bytes);
--- opencv/src/main/java/org/opencv/face/video/ImageConverter.java
+++ opencv/src/main/java/org/opencv/face/video/ImageConverter.java
@@ -2,10 +2,7 @@ package org.opencv.face.video;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
-import java.io.ByteArrayInputStream;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
-import static org.opencv.imgcodecs.Imgcodecs.imencode;
 
 /**
  *
@@ -16,23 +13,24 @@ public final class ImageConverter {
     private ImageConverter() {
     }
 
-    public static BufferedImage toBufferedImage(Mat matrix) {
-        int type = BufferedImage.TYPE_BYTE_GRAY;
-        if (matrix.channels() > 1) {
-            type = BufferedImage.TYPE_3BYTE_BGR;
-        }
-        byte[] bytes = readPixel(matrix);
-        BufferedImage image = new BufferedImage(matrix.cols(), matrix.rows(), type);
+    public static BufferedImage toBufferedImage(final Mat matrix) {
+        final BufferedImage image = new BufferedImage(matrix.cols(), matrix.rows(), getType(matrix));
         final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        byte[] bytes = readPixel(matrix);
         System.arraycopy(bytes, 0, targetPixels, 0, bytes.length);
         return image;
     }
 
-    private static byte[] readPixel(Mat matrix) {
+    private static int getType(final Mat matrix) {
+        if (matrix.channels() > 1) {
+            return BufferedImage.TYPE_3BYTE_BGR;
+        }
+        return BufferedImage.TYPE_BYTE_GRAY;
+    }
+
+    private static byte[] readPixel(final Mat matrix) {
         byte[] bytes = new byte[matrix.channels() * matrix.cols() * matrix.rows()];
         matrix.get(0, 0, bytes); // get all the pixels
         return bytes;
     }
-
-
 }
