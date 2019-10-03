3eacadac0c18c5077f01168541a714d684a90af0
Mario <mario-s@users.noreply.github.com>
2019-10-03 21:10:57 +0200

    Kotlin DSL (#3)
    * one src folder with kotlin dsl
    
    * call copy before process resources
    
    * update readme

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    java
+    application
+    jacoco
+}
+
+repositories {
+    jcenter()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+application {
+    mainClassName = "org.javacv.Main"
+}
+
+jacoco {
+    toolVersion = "0.8.4"
+}
+
+tasks.processResources {
+    dependsOn("copyTrainToMain", "copyTrainToTest")
+}
+
+dependencies {
+        //JavaCV and more
+    implementation("org.bytedeco:javacv:1.4")
+    implementation("org.bytedeco.javacpp-presets:opencv:3.4.3-1.4.3")
+    implementation("org.bytedeco.javacpp-presets:opencv:3.4.3-1.4.3:windows-x86_64")
+    implementation("org.bytedeco.javacpp-presets:opencv:3.4.3-1.4.3:macosx-x86_64")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("com.google.code.gson:gson:2.8.1")
+    implementation("com.google.guava:guava:27.0.1-jre")
+
+    // Use JUnit test framework
+    testImplementation("junit:junit:4.12")
+}
+
+tasks {
+    wrapper {
+        gradleVersion = "5.6.2"
+    }
+
+    val target = "$buildDir/resources/%s/org/javacv/train"
+    val source = "$rootDir/data/train"
+    register<Copy>("copyTrainToMain") {
+        from(source)
+        include("*.*")
+        into(String.format("$target", "main"))
+    }
+    register<Copy>("copyTrainToTest") {
+        from(source)
+        include("*.*")
+        into(String.format("$target", "test"))
+    }
+}
--- src/main/java/org/javacv/Main.java
+++ src/main/java/org/javacv/Main.java
@@ -7,10 +7,10 @@ public class Main {
 
 
     public static void main(String[] args) {
-        CanvasDemo demo = new CanvasDemo();
-        demo.run();
+        //CanvasDemo demo = new CanvasDemo();
+        //demo.run();
 
-        //VideoWindow.launch();
+        VideoWindow.launch();
     }
 
 
