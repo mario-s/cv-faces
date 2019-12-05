ddc512e15626d443342feec20e6a09cff78d19db
Mario-S <spindizzy@outlook.com>
2019-12-05 17:58:53 +0100

    extend dnn

--- src/main/java/org/javacv/detect/face/dnn/DnnDetector.java
+++ src/main/java/org/javacv/detect/face/dnn/DnnDetector.java
@@ -1,6 +1,7 @@
 package org.javacv.detect.face.dnn;
 
 import org.bytedeco.javacpp.indexer.FloatIndexer;
+import org.bytedeco.javacpp.opencv_imgproc;
 import org.javacv.detect.AbstractDetector;
 import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_core.Point;
@@ -8,6 +9,9 @@ import org.bytedeco.javacpp.opencv_core.Rect;
 import org.bytedeco.javacpp.opencv_core.Scalar;
 import org.bytedeco.javacpp.opencv_core.Size;
 import org.bytedeco.javacpp.opencv_dnn.Net;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import static org.bytedeco.javacpp.opencv_imgproc.rectangle;
 import static org.javacv.common.ImageUtil.resize;
@@ -19,6 +23,9 @@ import static org.bytedeco.javacpp.opencv_dnn.blobFromImage;
  * Detector which uses a pretrained model to detect faces.
  */
 public class DnnDetector extends AbstractDetector {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DnnDetector.class);
+
     private static final double THRESHOLD = 0.8;
     private static final double SCALE_FACTOR = 1.0;
 
@@ -44,7 +51,8 @@ public class DnnDetector extends AbstractDetector {
 
     @Override
     public long markObjects(Mat img) {
-        Mat output = forward(img);
+        Mat resized = resize(img, TARGET_SIZE);
+        Mat output = forward(resized);
         FloatIndexer indexer = createIndexer(output);
 
         long objects = 0;
@@ -52,8 +60,8 @@ public class DnnDetector extends AbstractDetector {
             float confidence = indexer.get(i, 2);
             if (confidence > THRESHOLD) {
                 objects++;
-
-                rectangle(img, getPos(indexer, i), color);
+                var pos = getPos(indexer, i);
+                rectangle(img, pos[0], pos[1], color);
             }
         }
 
@@ -62,8 +70,6 @@ public class DnnDetector extends AbstractDetector {
 
     //forward image to network
     private Mat forward(Mat img) {
-        resize(img, TARGET_SIZE);
-
         //create a 4-dimensional blob from image
         //for more details @see https://docs.opencv.org/trunk/d6/d0f/group__dnn.html#gabd0e76da3c6ad15c08b01ef21ad55dd8
         Mat input = blobFromImage(img, SCALE_FACTOR, TARGET_SIZE, MEAN, false, false, CV_32F);
@@ -79,11 +85,20 @@ public class DnnDetector extends AbstractDetector {
         return mat.createIndexer();
     }
 
-    private Rect getPos(FloatIndexer indexer, int index) {
-        int tx = (int)indexer.get(index, 3);
-        int ty = (int) indexer.get(index, 4);
-        int bx = (int) indexer.get(index, 5);
-        int by = (int) indexer.get(index, 6);
-        return new Rect(new Point(tx, ty), new Point(bx, by));
+    private Point[] getPos(FloatIndexer indexer, int index) {
+        int tx = scale(indexer.get(index, 3));
+        int ty = scale(indexer.get(index, 4));
+        int bx = scale(indexer.get(index, 5));
+        int by = scale(indexer.get(index, 6));
+
+        Point pt1 = new Point(tx, ty);
+        Point pt2 = new Point(bx, by);
+        LOG.debug("({},{})x({},{})", tx, ty, bx, by);
+
+        return new Point[] {pt1, pt2};
+    }
+
+    private int scale(float in) {
+        return (int) (in * 300);
     }
 }
