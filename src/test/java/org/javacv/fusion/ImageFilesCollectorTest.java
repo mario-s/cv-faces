61115d6cf4949cbfca83566b8374516cdcb849ca
Mario <ma.schroed73@gmail.com>
2022-09-18 21:36:30 +0200

    fix imports

--- src/test/java/org/javacv/fusion/ImageFilesCollectorTest.java
+++ src/test/java/org/javacv/fusion/ImageFilesCollectorTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
